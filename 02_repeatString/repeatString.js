5ffe564b05e0aa1bb4dc9500a2b426f55c4eb096
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-10-26 19:42:48 +0530

    solution to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  // error check for negative numbers
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  // variable to store string
+  stringArray = "";
+
+  // loop to add string repeats into variable
+  let i = 0;
+  while (i < num) {
+    stringArray += string;
+    i++;
+  }
+  return stringArray;
 };
 
 // Do not edit below this line
