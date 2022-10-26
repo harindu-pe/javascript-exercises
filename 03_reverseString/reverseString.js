6abfaae9cdc364ea992ee22197e30dafbb51d79d
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-10-26 19:59:58 +0530

    solution to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let inputString = string;
+  let inputStringLength = inputString.length;
+  let reverseString = "";
+  let i = 0;
 
+  // extract characters from string
+  while (i <= inputStringLength) {
+    // adding each character from the end of the input string
+    reverseString += inputString.charAt(inputStringLength - i);
+    i++;
+  }
+
+  return reverseString;
 };
 
 // Do not edit below this line
