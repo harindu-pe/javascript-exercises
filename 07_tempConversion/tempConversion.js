4c48a57eb103e797545a0776c8fe4d31eb1520d2
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-10-30 14:44:34 +0530

    solution to tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  let ans = ((num - 32) * 5) / 9;
+  ans = Math.round(ans * 10) / 10;
+  return ans;
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  let ans = num * (9 / 5) + 32;
+  ans = Math.round(ans * 10) / 10;
+  return ans;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
