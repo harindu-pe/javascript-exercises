5bffd03f3361378174143c3350e8a7e17c935e68
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-10-30 14:19:21 +0530

    solution to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function () {
+  // parse the arguments
+  let args = Array.from(arguments);
+  // sort in ascending order
+  args = args.sort();
+  // setting the limits
+  const start = args[0];
+  const stop = args[args.length - 1];
 
+  // error check
+  if (
+    typeof start !== "number" ||
+    typeof stop !== "number" ||
+    start < 0 ||
+    stop < 0
+  ) {
+    return "ERROR";
+  } else {
+    //initialize counter variable
+    let sumNum = 0;
+    // loop
+    for (let i = start; i <= stop; i++) {
+      sumNum += i;
+    }
+    // return sum
+    return sumNum;
+  }
 };
 
 // Do not edit below this line
