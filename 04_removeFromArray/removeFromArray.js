9f050fcd6e11a6159e821eb7bc8a355fc5d4c57d
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-10-30 13:56:34 +0530

    solution to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  const args = Array.from(arguments);
+  let newArray = array;
 
+  // iterate over the input array and filter
+  for (let i = 1; i < args.length; i++) {
+    newArray = newArray.filter((number) => number !== args[i]);
+  }
+
+  // return new array
+  return newArray;
 };
 
 // Do not edit below this line
