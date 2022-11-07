7311265fff48403e277d9d11ee224b31a4580ead
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-11-07 16:05:33 +0530

    solution to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function () {
+  return arguments[0] + arguments[1];
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+  return arguments[0] - arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+  const initialValue = 0;
+  const sum = array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  const multi = array.reduce(
+    (previousValue, currentValue) => previousValue * currentValue,
+    1
+  );
+  return multi;
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    let fact = 1;
+    for (let i = 1; i <= num; i++) {
+      fact = fact * i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
