116a278ab8a4990b53ac90b43f98be60aba534bc
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-11-07 20:34:41 +0530

    solution to getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  const titleArray = books.map((book) => {
+    return book.title;
+  });
 
+  return titleArray;
 };
 
 // Do not edit below this line
