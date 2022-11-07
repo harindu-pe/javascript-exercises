12d73058795feb4626ddd71757cab71398592327
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-11-07 20:27:01 +0530

    solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
 
+  const index = num;
+
+  let previousValue;
+  let currentValue;
+  let fib_num;
+  let fib_array = [0, 1];
+
+  for (let i = 0; i < num; i++) {
+    previousValue = fib_array[i];
+    currentValue = fib_array[i + 1];
+    fib_num = previousValue + currentValue;
+    fib_array.push(fib_num);
+  }
+  return fib_array[index];
 };
 
 // Do not edit below this line
