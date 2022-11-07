14ac9fb13de515aca510fb592e2d4cbb429d5447
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-11-07 16:32:04 +0530

    solution to palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // removing punctuation and word breaks
+  let cleanString = string.replace("!", "");
+  cleanString = cleanString.replace(/[^a-zA-Z ]/g, "");
+  cleanString = cleanString.replaceAll(" ", "");
 
+  // convert string to lowercase
+  let forwardString = cleanString.toLowerCase();
+
+  // split string to a new array
+  let splitString = forwardString.split("");
+
+  // reverse the array
+  let reverseArray = splitString.reverse();
+
+  // join array back to string
+  let joinArray = reverseArray.join("");
+
+  return forwardString === joinArray;
 };
 
 // Do not edit below this line
